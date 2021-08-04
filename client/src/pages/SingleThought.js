01735df702d3df0e875d4e7331778452c771bc9f
kotaewing <kota.ewing@gmail.com>
2021-08-04 16:00:24 -0600

    added reaction form

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionForm from '../components/ReactionForm';
 import ReactionList from '../components/ReactionList';
+
+import Auth from '../utils/auth';
+import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -32,6 +35,7 @@ const SingleThought = props => {
         </div>
       </div>
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -37,3 +37,33 @@ export const ADD_FRIEND = gql`
         }
     }
 `
+
+export const ADD_THOUGHT = gql`
+    mutation addThought($thoughtText: String!) {
+        addThought(thoughtText: $thoughtText) {
+            _id
+            thoughtText
+            username
+            createdAt
+            reactionCount
+            reactions {
+                _id
+            }
+        }
+    }
+`
+
+export const ADD_REACTION = gql`
+    mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+        addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+            _id
+            reactionCount
+            reactions {
+                _id
+                reactionBody
+                createdAt
+                username
+            }
+        }
+    }
+`
